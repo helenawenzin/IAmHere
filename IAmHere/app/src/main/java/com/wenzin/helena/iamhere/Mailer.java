e7f8c82288ed7ac68de8783badd854ce18691533
Helena Wenzin <helma254@hotmail.com>
2016-03-20 13:38:37 +0100

    Change of method for sending email, with Sendgrid

---
+++ IAmHere/app/src/main/java/com/wenzin/helena/iamhere/Mailer.java
@@ -0,0 +1,57 @@
+package com.wenzin.helena.iamhere;
+
+import android.os.AsyncTask;
+
+import com.sendgrid.*;
+
+import java.util.Hashtable;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by Helena on 2016-03-18.
+ */
+public class Mailer {
+
+    public void sendMail() {
+        String result = "";
+        Hashtable<String,String> params = new Hashtable<String,String>();
+        // Send the Email
+        SendEmailWithSendGrid sendEmailWithSendGrid = new SendEmailWithSendGrid();
+        try {
+            result = sendEmailWithSendGrid.execute(params).get();
+        } catch (InterruptedException e) {
+            // TODO Implement exception handling
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            // TODO Implement exception handling
+            e.printStackTrace();
+        }
+        System.out.println(result);
+    }
+
+    private class SendEmailWithSendGrid extends AsyncTask<Hashtable<String,String>, Void, String> {
+
+        @Override
+        protected String doInBackground(Hashtable<String,String>... params) {
+            //TODO: Use params instead of hardcoded values in email
+            Hashtable<String,String> h = params[0];
+            SendGrid sendgrid = new SendGrid("");
+
+            SendGrid.Email email = new SendGrid.Email();
+
+            email.addTo("helena.wenzin@gmail.com");
+            email.setFrom("helena.wenzin@gmail.com");
+            email.setSubject("Simon är nu hemma!");
+            email.setHtml("Hej! Nu har jag kommit hem! //Simon");
+
+            SendGrid.Response response = null;
+            try {
+                response = sendgrid.send(email);
+            } catch (SendGridException e) {
+                e.printStackTrace();
+            }
+
+            return response.toString();
+        }
+    }
+}
--- IAmHere/app/src/main/java/com/wenzin/helena/iamhere/MainActivity.java
+++ IAmHere/app/src/main/java/com/wenzin/helena/iamhere/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    Mailer mailer = new Mailer();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,17 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMailHomeButton(View view) {
-        System.out.println("Jag skickar mail... Hemma!");
-        Intent intent = new Intent(Intent.ACTION_SENDTO); // it's not ACTION_SEND
-       // final Intent intent = new Intent(android.content.Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_SUBJECT, "Jag är här");
-        intent.putExtra(Intent.EXTRA_TEXT, "Nu vill jag meddela var jag är... Hemma");
-        intent.setData(Uri.parse("mailto:helena.wenzin@gmail.com")); // or just "mailto:" for blank
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // this will make such that when user returns to your app, your app is displayed, instead of the email app.
-        startActivity(intent);
-
-
+        mailer.sendMail();
     }
 
     public void sendMailSchoolButton(View view) {
