3ec8e42bfb9cd97530365a383cb36c952c13e3e7
Helena Wenzin <helma254@hotmail.com>
2016-03-24 18:15:18 +0100

    added class settingsFromUser

---
+++ IAmHere/app/src/main/java/com/wenzin/helena/iamhere/settingsFromUser.java
@@ -0,0 +1,27 @@
+package com.wenzin.helena.iamhere;
+
+/**
+ * Created by Helena on 2016-03-24.
+ */
+public class settingsFromUser {
+
+    private String to = "";
+    private String from = "";
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+    public String getTo() {
+        return to;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+
+}
