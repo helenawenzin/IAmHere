7945761377e7ac7062041e02c3a2335f86743cc6
Helena Wenzin <helma254@hotmail.com>
2016-03-24 19:23:54 +0100

    hard coded settings attributes added in settings class

--- IAmHere/app/src/main/java/com/wenzin/helena/iamhere/settingsFromUser.java
+++ IAmHere/app/src/main/java/com/wenzin/helena/iamhere/settingsFromUser.java
@@ -3,10 +3,10 @@ package com.wenzin.helena.iamhere;
 /**
  * Created by Helena on 2016-03-24.
  */
-public class settingsFromUser {
+public class SettingsFromUser {
 
-    private String to = "";
-    private String from = "";
+    private String to = "helena.wenzin@gmail.com";
+    private String from = "helena.wenzin@gmail.com";
 
     public void setTo(String to) {
         this.to = to;
