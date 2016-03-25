889fab3c5cf0b3415a8f282821deb9d0692f23ef
Helena Wenzin <helma254@hotmail.com>
2016-03-25 10:15:52 +0100

    textfields for entering emails in settings added

--- IAmHere/app/src/main/java/com/wenzin/helena/iamhere/SettingsActivity.java
+++ IAmHere/app/src/main/java/com/wenzin/helena/iamhere/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -18,6 +19,9 @@ public class SettingsActivity extends AppCompatActivity {
     public void saveSettings(View view) {
 
         Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.save_Email_Send_To);
+        EditText editText2 = (EditText) findViewById(R.id.save_Email_Send_From);
+        //String messageEmail = editText.getText().toString();
         String message = "Settings är sparade";
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
