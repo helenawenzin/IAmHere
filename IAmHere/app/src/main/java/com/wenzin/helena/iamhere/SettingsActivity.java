8badf34bce1f08abc2421a6d6e69ea2a23e2b753
Helena Wenzin <helma254@hotmail.com>
2016-03-25 11:40:11 +0100

    added sharedPreferences to store email values

--- IAmHere/app/src/main/java/com/wenzin/helena/iamhere/SettingsActivity.java
+++ IAmHere/app/src/main/java/com/wenzin/helena/iamhere/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.wenzin.helena.iamhere;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.Preference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,19 +10,38 @@ import android.widget.EditText;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    public final static String EXTRA_MESSAGE = "com.wenzin.helena.iamhere.MESSAGE";
+    public static final String EXTRA_MESSAGE = "com.wenzin.helena.iamhere.MESSAGE";
+
+    public static final String MY_PREFS_NAME = "MyPrefsFile";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+
+        EditText textMailTo = (EditText) findViewById(R.id.save_Email_Send_To);
+        EditText textMailFrom = (EditText) findViewById(R.id.save_Email_Send_From);
+
+        textMailTo.setText(pref.getString("sendTo", ""));
+        textMailFrom.setText(pref.getString("sendFrom", ""));
     }
 
     public void saveSettings(View view) {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+
+        EditText textMailTo = (EditText) findViewById(R.id.save_Email_Send_To);
+        EditText textMailFrom = (EditText) findViewById(R.id.save_Email_Send_From);
+
+        editor.putString("sendTo", textMailTo.getText().toString());
+        editor.putString("sendFrom", textMailFrom.getText().toString());
+        editor.apply();
+
 
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.save_Email_Send_To);
-        EditText editText2 = (EditText) findViewById(R.id.save_Email_Send_From);
         //String messageEmail = editText.getText().toString();
         String message = "Settings är sparade";
         intent.putExtra(EXTRA_MESSAGE, message);
